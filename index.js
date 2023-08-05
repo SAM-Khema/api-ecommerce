24a325080ca6745dbdceb69a1cfe4b90d013f50b
SAM-Khema <kemasam2001@gmail.com>
2023-08-05 13:53:50 +0700

    cors

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const bodyParser = require('body-parser');
 const cors = require('cors')
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true
-}))
+// app.use(cors({
+//   origin: 'http://localhost:3000',
+//   credentials: true
+// }))
+app.use(cors('*'))
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
